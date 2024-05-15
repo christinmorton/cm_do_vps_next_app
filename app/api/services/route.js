fc3eac0409a073cb254c3f81524b64c2be73db70
unknown <sysadmin@christinmorton.com>
2024-05-15 14:34:31 -0700

    dynamic vs static route...

--- app/api/services/route.js
+++ app/api/services/route.js
@@ -1,10 +1,12 @@
 import { NextResponse } from 'next/server'
 
+export const dynamic = 'force-dynamic'
+
 import services from '@/assets/data/services.json'
 
 export async function GET () {
     //simulate fetching data from a database
-    const timeout = 10000; // Set your desired timeout (in milliseconds)
+    const timeout = 2000; // Set your desired timeout (in milliseconds)
 
     setTimeout(() => {
         console.log('Fetching data from the database...')
