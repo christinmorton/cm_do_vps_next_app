4d3f83e91694145afcd385c22947ee96c6fd6991
unknown <sysadmin@christinmorton.com>
2024-05-31 21:06:47 -0700

    attempting to pass build for continuous integration pt3.

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import SiteNavigation from "@/components/SiteNavigation";
 import Footer from "@/components/Footer";
 import Modal from "@/components/Modal";
 import ModalWidget from "@/components/ModalWidget";
+import { Suspense } from "react";
 
 export const metadata = {
   title: "Christin Morton",
@@ -15,11 +16,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <SiteNavigation />
+        <Suspense fallback={<div>Loading...</div>}>
+          <SiteNavigation />
+        </Suspense>
         {children}
         <Footer />
-        <Modal />
-        <ModalWidget queryParam={'modalWidget'} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Modal />
+        </Suspense>
+        <Suspense fallback={<div>Loading...</div>}>
+          <ModalWidget queryParam={'modalWidget'} />
+        </Suspense>
       </body>
     </html>
   );
